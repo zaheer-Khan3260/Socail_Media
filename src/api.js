b7851e3ca573baa85e16c079b98276362768301a
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-06-28 22:17:54 +0530

    _

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-console.log('Backend Server URL:', process.env.REACT_APP_BACKEND_SERVER_URL);
+console.log('Backend server url:', process.env.REACT_APP_BACKEND_SERVER_URL);
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_SERVER_URL, 
   withCredentials: true 
